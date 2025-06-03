89c27a15558cc1e3d364ef0267005b6a6d806de7
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-06-02 20:59:32 -0500

    feat: detailed artist view with song count and songs

--- tunaapi/models/song.py
+++ tunaapi/models/song.py
@@ -4,6 +4,7 @@ from .artist import Artist
 
 class Song(models.Model):
     title = models.CharField(max_length=200)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    artist = models.ForeignKey(
+        Artist, on_delete=models.CASCADE, related_name='songs')
     album = models.CharField(max_length=200)
     length = models.PositiveIntegerField()
